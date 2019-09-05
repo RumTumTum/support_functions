d02b2c6640a813421db0d6cd64b9be1d4854a9d3
RumTumTum <49051718+RumTumTum@users.noreply.github.com>
2019-09-05 10:06:57 -0400

    first commit

---
+++ regression_support.py
@@ -0,0 +1,16 @@
+
+# import relevant libraries
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+
+
+def time_series_tools():
+    """print names of relevant tools"""
+    print(
+        "useful tools:",
+        "hello",
+        "world",
+        sep="\n"
+    )
